b8e63a49fca88d03468cdf4479427bc21460f886
Diego Alonso Ojeda Medina <dojedam@sena.edu.co>
2020-09-15 18:24:59 -0500

    * Correccion en el nombre del usuario que inicia session

--- views/partials/sliderbar_main_menu.php
+++ views/partials/sliderbar_main_menu.php
@@ -17,7 +17,7 @@
                 <img src="<?= $baseURL ?>/views/public/img/user.png" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Diego Ojeda</a>
+                <a href="#" class="d-block"> <?= $_SESSION['UserInSession']['nombres'] ?> </a>
             </div>
         </div>
 

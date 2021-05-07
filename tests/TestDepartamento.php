a21d18265c4f84d26618109e4234a0a2a22502d4
Diego Alonso Ojeda Medina <dojedam@sena.edu.co>
2021-05-07 11:51:53 -0500

    Test Departamentos

---
+++ tests/TestDepartamento.php
@@ -0,0 +1,15 @@
+<?php
+
+//require ("..\app\Models\Departamentos.php");
+//use App\Models\Departamentos;
+
+//$dep = Departamentos::searchForId(15);
+
+//$arrMunicipiosBoyaca = $dep->getMunicipiosDepartamento();
+/* @var $arrMunicipiosBoyaca \App\Models\Municipios[] */
+//foreach ($arrMunicipiosBoyaca as $Municipio){
+//    echo $Municipio->getId()." - ".$Municipio->getNombre()."\n";
+//}
+
+//$Municipio = $Municipio::searchForId(15759); //Buscar Municipio de Sogamoso
+//print_r($Municipio->getDepartamento()->getNombre()); //Municipios Hermanos de Sogamoso
